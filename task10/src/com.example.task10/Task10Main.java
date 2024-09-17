b17240a6f65f7b53e5ce77b4eb5f73c3cf223679
Alexander <itskilobyte@gmail.com>
2024-09-17 22:49:17 +0500

    Completed 10th task.

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,17 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int result = arr[0];
+        int resultIndex = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= result) {
+                result = arr[i];
+                resultIndex = i;
+            }
+        }
+
+        return resultIndex;
     }
 
 }
